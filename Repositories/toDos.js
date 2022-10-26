1d5bbe079731007d7c8f2d4f35780d3bc598fe18
Malika Bakhtiyorova <malikabakhtiyorova0@gmail.com>
2022-10-26 12:37:16 +0500

    format json files

--- Repositories/toDos.js
+++ Repositories/toDos.js
@@ -11,7 +11,7 @@ async function postToDo (body) {
   let data = await readFile('./DB/todos.json')
   data = JSON.parse(data.toString())
   data.push(JSON.parse(body))
-  await writeFile('./DB/todos.json', JSON.stringify(data))
+  await writeFile('./DB/todos.json', JSON.stringify(data, undefined, 2))
 }
 
 async function updateToDo (body, url) {
@@ -20,28 +20,28 @@ async function updateToDo (body, url) {
   data = JSON.parse(data.toString())
 
   data.find((d) => url[2] == d.id).text = JSON.parse(body).text
-  await writeFile('./DB/todos.json', JSON.stringify(data))
+  await writeFile('./DB/todos.json', JSON.stringify(data, undefined, 2))
 }
 
 async function deleteToDo (url) {
   let data = await readFile('./DB/todos.json')
   data = JSON.parse(data.toString())
   data.find((d) => url[2] == d.id).deleted = true
-  await writeFile('./DB/todos.json', JSON.stringify(data))
+  await writeFile('./DB/todos.json', JSON.stringify(data, undefined, 2))
 }
 
 async function changeToDoStatus (url) {
   let data = await readFile('./DB/todos.json')
   data = JSON.parse(data.toString())
   data.find((d) => url[3] == d.id).status = url[4]
-  await writeFile('./DB/todos.json', JSON.stringify(data))
+  await writeFile('./DB/todos.json', JSON.stringify(data, undefined, 2))
 }
 
 async function assignTo (body) {
   let data = await readFile('./DB/todos.json')
   data = JSON.parse(data.toString())
   data.find((d) => JSON.parse(body).task_id == d.id).assignee = JSON.parse(body).user_id
-  await writeFile('./DB/todos.json', JSON.stringify(data))
+  await writeFile('./DB/todos.json', JSON.stringify(data, undefined, 2))
 }
 
 module.exports = {
